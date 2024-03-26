895b78cddb56f4ca4bc79a9628f3b17fdfa3fcc8
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-03-26 00:11:39 -0400

    ready event

---
+++ src/events/client/ready.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const chalk = require('chalk');
+
+module.exports = async (client) => {
+  const readyLogs = new Discord.WebhookClient({
+    url: client.webhooks.readyLogs.url,
+  });
+
+  const username = client.user.username;
+
+  readyLogs.send({
+    username: 'Logger',
+    embeds: [
+      new Discord.EmbedBuilder()
+        .setTitle(`🔋 ${username} is ready!`)
+        .setTimestamp(),
+    ],
+  });
+
+  const activities = [
+    { name: `${client.guilds.cache.size} Servers`, type: 2 }, // LISTENING
+    { name: `${client.channels.cache.size} Channels`, type: 0 }, // PLAYING
+    { name: `${client.users.cache.size} Users`, type: 3 }, // WATCHING
+  ];
+  const status = ['online', 'dnd', 'idle'];
+
+  let i = 0;
+  setInterval(() => {
+    if (i >= activities.length) i = 0;
+    client.user.setActivity(activities[i]);
+    i++;
+  }, 5000);
+
+
+  console.log(
+    chalk.blue(chalk.bold(`System`)),
+    chalk.white(`>>`),
+    chalk.red(`${username}`),
+    chalk.green(`is ready!`)
+  );
+};
