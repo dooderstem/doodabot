cb2fdc475b8c814fe3e874a33933825c7122d3a5
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-03-26 00:12:50 -0400

    Update interactionCreate.js

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 module.exports = async (client, interaction) => {
   if (interaction.isChatInputCommand() || interaction.isContextMenu()) {
+    const cmd = client.interactions.get(interaction.commandName);
+
     cmd.run(client, interaction, interaction.options._hoistedOptions);
   }
 };
