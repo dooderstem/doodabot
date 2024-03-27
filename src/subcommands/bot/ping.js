d7d2ce909b90dcb3ab411fb2fff0df7facb3158f
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-03-27 00:55:12 -0400

    Update ping.js

--- src/subcommands/bot/ping.js
+++ src/subcommands/bot/ping.js
@@ -1,5 +1,49 @@
 const Discord = require('discord.js');
 
 module.exports = async (client, interaction, args) => {
-  interaction.reply('pong');
+  interaction.reply('Pong!');
+  
+  //   client
+  //     .simpleEmbed(
+  //       {
+  //         desc: `${client.emotes.animated.loading} Calculating ping...`,
+  //         type: 'editreply',
+  //       },
+  //       interaction
+  //     )
+  //     .then((resultMessage) => {
+  //       const ping = Math.floor(
+  //         resultMessage.createdTimestamp - interaction.createdTimestamp
+  //       );
+  //       mongoose.connection.db.admin().ping(function (err, result) {
+  //         var mongooseSeconds = (result.ok % 60000) / 1000;
+  //         var pingSeconds = (ping % 60000) / 1000;
+  //         var apiSeconds = (client.ws.ping % 60000) / 1000;
+  //         client.embed(
+  //           {
+  //             title: `${client.emotes.normal.pong}・Pong`,
+  //             desc: `Check out how fast our bot is`,
+  //             fields: [
+  //               {
+  //                 name: '<:discord_bot:1012038552521031703> ┆ Bot latency',
+  //                 value: `${ping}ms (${pingSeconds}s)`,
+  //                 inline: true,
+  //               },
+  //               {
+  //                 name: '<:blue_hammers:1012018248163786763> ┆ API Latency',
+  //                 value: `${client.ws.ping}ms (${apiSeconds}s)`,
+  //                 inline: true,
+  //               },
+  //               {
+  //                 name: '<:to_space:1012038751729491968> ┆ Database Latency',
+  //                 value: `${result.ok}ms (${mongooseSeconds}s)`,
+  //                 inline: true,
+  //               },
+  //             ],
+  //             type: 'editreply',
+  //           },
+  //           interaction
+  //         );
+  //       });
+  //     });
 };
