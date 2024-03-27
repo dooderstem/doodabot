961be0031c96d9d94b1cee2a37f23cf71d8a89bd
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-03-27 15:57:45 -0400

    Restructure command handler

--- src/handlers/loaders/commands.js
+++ src/handlers/loaders/commands.js
@@ -4,6 +4,35 @@ const { Routes } = require('discord-api-types/v9');
 const chalk = require('chalk');
 const fs = require('fs');
 
+module.exports = async (client) => {
+  const interactions = [];
+
+  // Load interactions
+  const slash = loadFromDirectory(
+    client,
+    './src/interactions',
+    client.interactions
+  );
+
+  slash.each((cmd) => interactions.push(cmd.data));
+
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+  (async () => {
+    try {
+      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+        body: interactions,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+
+  // Load commands
+  const cmds = loadFromDirectory(client, './src/commands', client.commands);
+
+  cmds.each((cmd) => client.commands.set(cmd.data.name, cmd));
+};
+
 function loadFromDirectory(client, directory, collection) {
   fs.readdirSync(directory).forEach((dirs) => {
     const files = fs
@@ -17,29 +46,3 @@ function loadFromDirectory(client, directory, collection) {
   });
   return collection;
 }
-
-module.exports = async (client) => {
-  const commands = [];
-  const interactions = [];
-
-  const slash = loadFromDirectory(
-    client,
-    './src/interactions',
-    client.interactions
-  );
-  slash.each((cmd) => interactions.push(cmd.data));
-
-  const cmds = loadFromDirectory(client, './src/commands', client.commands);
-  cmds.each((cmd) => commands.push(cmd.data));
-
-  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-  (async () => {
-    try {
-      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-        body: interactions,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  })();
-};
