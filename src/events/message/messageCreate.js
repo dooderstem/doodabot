ee6c987ee7548cc1818b0160da30377cda9923b9
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-03-27 15:55:56 -0400

    init messageCreate event

---
+++ src/events/message/messageCreate.js
@@ -0,0 +1,113 @@
+const Discord = require('discord.js');
+
+module.exports = async (client, message) => {
+  const msg = message;
+  const discordConfig = client.config.discord;
+  if (msg.author.bot) return;
+
+  const mentionBuilder = {
+    embeds: [
+      new Discord.EmbedBuilder()
+        .setTitle(`Hi, i'm doodabot!`)
+        .setDescription(
+          'Sit do in mollit ipsum velit proident amet consequat ullamco do cupidatat.'
+        )
+        .addFields(
+          {
+            name: '📢┆Alert!',
+            value:
+              'Exercitation incididunt excepteur magna anim consequat sunt voluptate deserunt ullamco dolor cupidatat. Do exercitation laboris do nisi dolore ipsum veniam amet consectetur esse commodo veniam eiusmod velit.',
+            inline: false,
+          },
+          {
+            name: '📨┆Invite me',
+            value: `Invite Bot in your own server! [Click here](${discordConfig.botInvite})`,
+          },
+          {
+            name: '❓┆Need support?',
+            value: `For questions you can join our [support server](${discordConfig.serverInvite})!`,
+          },
+          {
+            name: '🐞┆Found a bug?',
+            value: `Report all bugs via: \`/report bug\`!`,
+          }
+        )
+        .setColor(client.config.colors.normal),
+    ],
+    components: [
+      new Discord.ActionRowBuilder().addComponents(
+        new Discord.ButtonBuilder()
+          .setLabel('Invite')
+          .setURL(`${discordConfig.botInvite}`)
+          .setStyle(5),
+        new Discord.ButtonBuilder()
+          .setLabel('Support server')
+          .setURL(`${discordConfig.serverInvite}`)
+          .setStyle(5)
+      ),
+    ],
+  };
+
+  if (msg.guild) {
+    let row = await client.database.queryAsync(
+      `SELECT * FROM guild WHERE Guild = '${msg.guild.id}';`
+    );
+
+    let guildSettings = row[0];
+
+    if (!guildSettings) {
+      await client.database.queryAsync(
+        `INSERT INTO guild (Guild, Prefix) VALUES ('${msg.guild.id}', '${discordConfig.prefix}');`
+      );
+
+      row = await client.database.queryAsync(
+        `SELECT * FROM guild WHERE Guild = '${msg.guild.id}';`
+      );
+      guildSettings = row[0];
+    }
+
+    if (!guildSettings || !guildSettings.Prefix) {
+      row = await client.database.queryAsync(
+        `UPDATE guild SET Prefix = '${discordConfig.prefix}' WHERE Guild = '${msg.guild.id}';`
+      );
+      guildSettings = row[0];
+    }
+
+    let pfx;
+
+    if (!guildSettings || !guildSettings.Prefix) pfx = discordConfig.prefix;
+    else pfx = guildSettings.Prefix;
+
+    const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+    const prefixRegex = new RegExp(
+      `^(<@!?${client.user.id}>|${escapeRegex(pfx)})\\s*`
+    );
+
+    if (!prefixRegex.test(msg.content.toLowerCase())) return;
+
+    const [, matchedPrefix] = msg.content.toLowerCase().match(prefixRegex);
+
+    const args = message.content
+      .slice(matchedPrefix.length)
+      .trim()
+      .split(/ +/g);
+
+    const cmdName = args.shift().toLowerCase();
+    
+    const cmd = client.commands.get(cmdName);
+
+    if (!cmd) return;
+    else await cmd.run(client, message, args);
+
+    if (
+      msg.mentions.users.first() &&
+      msg.mentions.users.first().id == client.user.id &&
+      cmdName.length === 0
+    ) {
+      msg.react('👑');
+      msg.react('⚔️');
+      msg.channel.send(mentionBuilder);
+    }
+  }
+};
