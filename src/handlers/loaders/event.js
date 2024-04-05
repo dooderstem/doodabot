8d194a4356105f015852fdd61e0a11b0e0897187
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-04-04 22:03:34 -0400

    Update event.js

--- src/handlers/loaders/event.js
+++ src/handlers/loaders/event.js
@@ -1,18 +1,17 @@
 import fs from 'fs';
 
 export default function loadEvents(client) {
-  fs.readdirSync('./src/events').forEach((dirs) => {
+  fs.readdirSync('./src/events').forEach(async (dirs) => {
     const events = fs
       .readdirSync(`./src/events/${dirs}`)
       .filter((files) => files.endsWith('.js'));
 
     for (const file of events) {
-      (async () => {
-        const event = (await import(`../../events/${dirs}/${file}`)).default;
+      const { default: event } = await import(`../../events/${dirs}/${file}`);
+      if (event && typeof event === 'function')
         client
           .on(file.split('.')[0], event.bind(null, client))
           .setMaxListeners(0);
-      })();
     }
   });
 }
