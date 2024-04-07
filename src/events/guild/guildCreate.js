ebde87ea467cc8d8e01f4f2d0c1b889e482ead28
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-04-07 15:03:10 -0400

    Update guildCreate.js

--- src/events/guild/guildCreate.js
+++ src/events/guild/guildCreate.js
@@ -2,11 +2,11 @@ import Discord from 'discord.js';
 
 export default async (client, guild) => {
   let row = await client.database.queryAsync(
-    `SELECT \`Guild\` FROM \`guild\` WHERE \`Guild\` = '${guild.id}';`
+    `SELECT Guild FROM guild WHERE Guild = '${guild.id}';`
   );
 
   if (!row[0])
     await client.database.queryAsync(
-      `INSERT INTO \`guild\` (\`Guild\`) VALUES ('${guild.id}');`
+      `INSERT INTO guild (Guild) VALUES ('${guild.id}');`
     );
 };
