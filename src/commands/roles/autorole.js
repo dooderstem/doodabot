25145fa27925424e8ef07be63342c06f20b3704c
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-04-08 06:58:10 -0400

    Update autorole.js

--- src/commands/roles/autorole.js
+++ src/commands/roles/autorole.js
@@ -17,8 +17,33 @@ export default {
 
     if (!args.length) {
       if (!autorole_enabled && !autorole_role) {
-        return msg.reply('Please set an autorole.');
+        return msg.channel.send({
+          embeds: [
+            new Discord.EmbedBuilder()
+              .setTitle('📂┆ No autorole set.')
+              .setColor(bot.config.colors.normal),
+          ],
+        });
       }
     }
+
+    const findByRoleID = args[0].match(/(?<=<@&)(.*?)(?=>)/)
+      ? args[0].match(/(?<=<@&)(.*?)(?=>)/)[0]
+      : null;
+
+    const findByRoleName = msg.guild.roles.cache.find(
+      (role) => role.name === args[0]
+    );
+
+    let roleName;
+    if (findByRoleID) {
+      roleName = await msg.guild.roles.fetch(findByRoleID);
+      console.log(roleName.name);
+    } else if (findByRoleName) {
+      roleName = findByRoleName.name;
+      console.log(roleName);
+    }
+
+    // console.log(roleID);
   },
 };
