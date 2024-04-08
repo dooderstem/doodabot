411e05a07d640b72fd5b2f0a1cceb145ce33bd18
DOODER(Michael Smith) <skyviewsmith@gmail.com>
2024-04-07 20:29:36 -0400

    Update commands.js

--- src/handlers/loaders/commands.js
+++ src/handlers/loaders/commands.js
@@ -1,14 +1,15 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
+import chalk from 'chalk';
 
-export default async (client) => {
+export default async (bot) => {
   const interactions = [];
 
   // Load interactions
-  const slash = await loadCommands('./src/interactions', client.interactions);
+  const slash = await loadCommands('./src/interactions', bot.interactions);
 
   slash.each((cmd) => interactions.push(cmd.data));
 
@@ -25,19 +26,34 @@ export default async (client) => {
   })();
 
   // Load commands
-  await loadCommands('./src/commands', client.commands);
+  await loadCommands('./src/commands', bot.commands);
 };
 
 async function loadCommands(directory, collection) {
   const dirs = fs.readdirSync(directory);
+
   for (const dir of dirs) {
     const files = fs
       .readdirSync(path.join(directory, dir))
       .filter((file) => file.endsWith('.js'));
+
     for (const file of files) {
       const cmdFile = path.join(directory, dir, file);
-      const { default: command } = await import(`../../../${cmdFile}`);
-      collection.set(command.data.name, command);
+      const { default: cmd } = await import(`../../../${cmdFile}`);
+
+      if (
+        cmd.data instanceof SlashCommandBuilder &&
+        cmd.data.name.length >= 2 &&
+        typeof cmd.run == 'function'
+      )
+        collection.set(cmd.data.name, cmd);
+      else if (
+        cmd.data &&
+        cmd.data.name.length >= 2 &&
+        typeof cmd.run == 'function'
+      ) {
+        collection.set(cmd.data.name, cmd);
+      }
     }
   }
   return collection;
